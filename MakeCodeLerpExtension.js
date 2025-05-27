8da2ed54df912afbfd6264ec2955ad9665f1b9f8
okanagan-code-academy <157773119+okanagan-code-academy@users.noreply.github.com>
2025-05-27 13:06:17 -0700

    Create MakeCodeLerpExtension.js

---
+++ MakeCodeLerpExtension.js
@@ -0,0 +1,24 @@
+//% weight=100 color=#8E2EC4 icon=""
+namespace Math {
+    /**
+    * Linear interpolation between two values
+    * @param value0 is a real number, eg: 25.44
+    * @param value1 is a different real number, eg: 52.14
+    */
+    //% block
+    export function lerp(value0: number, value1: number, t: number): number {
+        return value0 + t * (value1 - value0);
+    }
+    /**
+    * Linear interpolation between two values, in particular with angles
+    * as angles are restricted between the interval [-Math.PI, Math.PI]
+    * @param value0 is a real number, eg: 25.44
+    * @param value1 is a different real number, eg: 52.14
+    */
+    //% block
+    export function lerpAngle(value0: number, value1: number, t: number): number {
+        let horizontalComponent = (1 - t) * Math.cos(value0) + t * Math.cos(value1);
+        let verticalComponent = (1 - t) * Math.sin(value0) + t * Math.sin(value1);
+        return Math.atan2(verticalComponent, horizontalComponent);
+    }
+}
